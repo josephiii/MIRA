3177578bdb24f032d5c16d7c200dc70145b79ec9
Joseph Ales <162182688+josephiii@users.noreply.github.com>
2026-06-13 21:21:33 -0400

    add Meta SDK to branch

--- frontend/Mobile/app/build.gradle.kts
+++ frontend/Mobile/app/build.gradle.kts
@@ -42,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation(libs.mwdat.core)
+    implementation(libs.mwdat.camera)
+    implementation(libs.mwdat.mockdevice)
 }
--- frontend/Mobile/settings.gradle.kts
+++ frontend/Mobile/settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google {
@@ -14,11 +16,24 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+val localProperties = Properties().apply {
+    val f = rootDir.resolve("local.properties")
+    if (f.exists()) f.inputStream().use { load(it) }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
+            credentials {
+                username = ""
+                password = System.getenv("GITHUB_TOKEN") ?: localProperties.getProperty("github_token")
+            }
+        }
     }
 }
 
