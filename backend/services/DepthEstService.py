5d233f9db8512bbbe6e21cb5d0ff1af717fe3711
“Jaurelus” <hrqrdbrdzf@privaterelay.appleid.com>
2026-07-02 11:39:48 -0400

    Depth Estimation File Structure

---
+++ backend/services/DepthEstService.py
@@ -0,0 +1,16 @@
+
+class DepthEstServices:
+    
+    def resizeImage():
+        """ Depth Anything V2 will force resize any image to 518px and resize anything above that.
+        By handling this part on our own, we make sure the model doesn't have to waste any extra
+        time or energy doing it.
+        """
+        return 
+    
+    def compressImageQuality():
+        return
+    def runModel():
+        return
+    def returnAudio():
+        return
